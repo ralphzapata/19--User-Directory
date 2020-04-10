7cb6334c8403d8463bfa461b3967d74928d2a3a5
Ralph Zapata <ralph_zapata@yahoo.com>
2020-04-10 13:34:27 +0930

    adding react framework

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Employee Directory</h1>
+      <p>Please search employee name.</p>
+    </div>
+  );
+}
+
+export default Header;
